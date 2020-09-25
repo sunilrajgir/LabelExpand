17ea496126b7764491f7d85d3356f55c10199469
Sunil Kumar <sunilrajgir@gmail.com>
2020-09-25 09:02:21 +0530

    Must Autolayout
    Expand label up to max width and then expand label up to maximum height then show ....

---
+++ POCLabelExpand/POCLabelExpand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POCLabelExpand
+//
+//  Created by sunil.kumar1 on 9/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
